b1376e677dda78a60c62244901e41a533d43b6ff
JBWERE-coder <lutherwere@gmail.com>
2023-10-06 10:00:57 +0300

    changes

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <section className="py-8">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        <p className="text-gray-700">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis ligula ac lectus
-          suscipit ultricies. Vestibulum suscipit nunc id lacus varius, nec bibendum ex
-          condimentum.
-        </p>
-      </div>
-    </section>
+    <section className="container mx-auto py-16">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="text-center md:text-left">
+            <h2 className="text-3xl font-bold">About Me</h2>
+            <p className="text-gray-700 mt-4">
+              I am a passionate software engineer dedicated to creating interactive web applications and solving everyday problems. With a strong foundation in web development, I thrive on tackling new challenges and delivering innovative solutions.
+            </p>
+          </div>
+          <div className="text-center">
+          <img src="C:\Users\HP\Downloads\WhatsApp Image 2023-09-13 at 22.30.01.jpeg" alt="Your Name" />
+          </div>
+        </div>
+      </section>
   );
 };
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,52 @@
 import React from 'react';
 import Link from 'next/link';
 
+// const Navbar: React.FC = () => {
+//   return (
+//     <header className="bg-blue-500 py-4">
+//       <div className="container mx-auto flex justify-between items-center">
+//         <Link href="/">
+//           <span className="text-gold text-2xl font-bold cursor-pointer">Jacob Were</span>
+//         </Link>
+//         <nav className="flex space-x-4">
+//           <Link href="/about">
+//             <span className="text-black hover:text-gold cursor-pointer ml-4">About</span>
+//           </Link>
+//           <Link href="/portfolio">
+//             <span className="text-black hover:text-gold cursor-pointer ml-4">Portfolio</span>
+//           </Link>
+//           <Link href="/contact">
+//             <span className="text-black hover:text-gold cursor-pointer mr-44">Contact</span>
+//           </Link>
+//           {/* Additional details in the navbar
+//           <span className="text-gray-100 text-sm bg-black p-2 rounded ml-4 hidden md:inline">
+//             Email: jacob@example.com
+//           </span>
+//           <span className="text-gray-100 text-sm bg-black p-2 rounded ml-4 hidden md:inline">
+//             Phone: +123-456-7890
+//           </span> */}
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// };
+
 const Navbar: React.FC = () => {
   return (
-    <header className="bg-blue-500 py-4">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className="bg-blue-500">
+      <div className="bg-blue-500">
         <Link href="/">
-          <span className="text-gold text-2xl font-bold cursor-pointer">Jacob Were</span>
+          <span>Jacob Were</span>
         </Link>
-        <nav className="flex space-x-4">
+        <nav>
           <Link href="/about">
-            <span className="text-black hover:text-gold cursor-pointer ml-4">About</span>
+            <span>About</span>
           </Link>
           <Link href="/portfolio">
-            <span className="text-black hover:text-gold cursor-pointer ml-4">Portfolio</span>
+            <span>Portfolio</span>
           </Link>
           <Link href="/contact">
-            <span className="text-black hover:text-gold cursor-pointer mr-44">Contact</span>
+            <span>Contact</span>
           </Link>
           {/* Additional details in the navbar
           <span className="text-gray-100 text-sm bg-black p-2 rounded ml-4 hidden md:inline">
